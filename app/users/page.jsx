313a364b9d9bc2580b24e44cdf6b5bb5284f9586
erichudson <defideveric@gmail.com>
2023-08-11 10:31:03 -0400

    user json

---
+++ app/users/page.jsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+    if (!res.ok) {
+        throw new Error ('Failed to fetch data')
+    }
+    return res.json()
+}
+
+
+
+export default async function UsersPage() {
+    const users = await getData()
+
+    console.log('Hello there');
+    
+    const content = (
+        <section>
+            <h2>
+                <Link href='/'>Back to Home</Link>
+            </h2>
+            <br/>
+            {users.map(user => {
+                return (
+                    <>
+                        <p key={user.id}>
+                        <Link href={`/users/${user.id}`}>{user.name}</Link>
+                        </p>
+                        <br/>
+                    </>
+                )
+            })}
+        </section>
+    )
+  return (
+    content
+  )
+}
